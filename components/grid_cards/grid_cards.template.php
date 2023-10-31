0790349be40c6f99d137c73bd46c8ade661db477
Carlos Krefer <carloskrefer@hotmail.com>
2023-10-30 21:17:39 -0300

    Refatoração do template do componente grid

--- components/grid_cards/grid_cards.template.php
+++ components/grid_cards/grid_cards.template.php
@@ -2,23 +2,32 @@
     $tipoGrid = $_SESSION['parametrosComponente'][0]; #template, avaliacao
 
     if ($tipoGrid == "template") {
+        imprimirCardsPaginaTemplates();
+    }
+
+
+
+    
+    function imprimirCardsPaginaTemplates() {
         echo <<<END
             <div class="grid_cards-container-cards"> 
         END; 
-            usarComponenteComParametros('card', ['novoTemplate']);
-
-            $result = buscarTodosTemplatesCadastrados();
-
-            while($row = mysqli_fetch_assoc($result)) {
-                usarComponenteComParametros('card', ['templateJaCadastrado', $row["titulo"], $row["data_hora_criacao"], $row["versao_checklist"], $row["autor_vesao"]]);
-            }   
-            // usarComponenteComParametros('card', ['templateJaCadastrado', 'Template Plano de Projeto', '01/01/2023', 'v1', 'Carlos Eduardo']);
-            // usarComponenteComParametros('card', ['templateJaCadastrado', 'Plano de Projeto', '01/01/2023', 'v1', 'Carlos Eduardo']);
-            // usarComponenteComParametros('card', ['templateJaCadastrado', 'Plano de Projeto', '01/01/2023', 'v1', 'Carlos Eduardo']);
-            // usarComponenteComParametros('card', ['templateJaCadastrado', 'Plano de Projeto', '01/01/2023', 'v1', 'Carlos Eduardo']);
-            // usarComponenteComParametros('card', ['templateJaCadastrado', 'Plano de Projeto', '01/01/2023', 'v1', 'Carlos Eduardo']);
+                imprimirCardCadastroNovoTemplate();
+                imprimirCardsTemplatesCadastrados();          
         echo <<<END
             </div>
         END; 
+    };
+
+    function imprimirCardCadastroNovoTemplate() {
+        usarComponenteComParametros('card', ['novoTemplate']);
+    }
+
+    function imprimirCardsTemplatesCadastrados() {
+        $result = buscarTodosTemplatesCadastrados();
+
+        while($row = mysqli_fetch_assoc($result)) {
+            usarComponenteComParametros('card', ['templateJaCadastrado', $row["titulo"], $row["data_hora_criacao"], $row["versao_checklist"], $row["autor_vesao"]]);
+        }  
     }
 ?>
