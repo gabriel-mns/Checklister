3fe4e4f7dd6d6735bff520b854f58db2122cc66b
Carlos Krefer <carloskrefer@hotmail.com>
2023-10-30 21:58:00 -0300

    Links navbar passam a diferenciar página ativa

--- components/barra_navegacao/barra_navegacao.template.php
+++ components/barra_navegacao/barra_navegacao.template.php
@@ -1,3 +1,16 @@
+<?php 
+  $rotaAtual = $_SERVER['REQUEST_URI'];
+  $classeLinkPaginaTemplatesAtivo = '';
+  $classeLinkPaginaAvaliacoesAtivo = '';
+
+  if (strpos($rotaAtual, 'index.php')) {
+    $classeLinkPaginaTemplatesAtivo = 'active';
+  }
+  if (strpos($rotaAtual, 'pagina_avaliacoes.php')) {
+    $classeLinkPaginaAvaliacoesAtivo = 'active';
+  } 
+?>
+
 <nav class="navbar navbar-expand-lg bg-body-tertiary bg-primary" data-bs-theme="dark">
   <div class="container-fluid">
     <a class="navbar-brand" href="../../paginas/pagina_principal/index.php">Checklister</a>
@@ -7,10 +20,10 @@
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="../../paginas/pagina_principal/index.php">Templates</a>
+          <a class="nav-link <?=$classeLinkPaginaTemplatesAtivo?>" aria-current="page" href="../../paginas/pagina_principal/index.php">Templates</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" aria-current="page" href="../../paginas/pagina_avaliacoes/pagina_avaliacoes.php">Avaliações</a>
+          <a class="nav-link <?=$classeLinkPaginaAvaliacoesAtivo?>" aria-current="page" href="../../paginas/pagina_avaliacoes/pagina_avaliacoes.php">Avaliações</a>
         </li>
     </div>
   </div>
