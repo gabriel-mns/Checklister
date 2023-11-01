7c638297ecedc63e4c4791170f1476e2b7ba39d7
Gabriel <gabrielgmns071204@gmail.com>
2023-10-31 22:26:55 -0300

    Adicionado +1 atributo possível ao template de input

--- components/input_text_form/input_text_form.template.php
+++ components/input_text_form/input_text_form.template.php
@@ -1,9 +1,10 @@
 <?php 
     $label = $_SESSION['parametrosComponente'][0];
     $identificador  = $_SESSION['parametrosComponente'][1];
+    $value = $_SESSION['parametrosComponente'][2] ?? '';
 ?>
 
 <div class="mb-3">
   <label for="<?= $identificador ?>" class="form-label input_text_form-label"><?= $label ?></label>
-  <input type="text" class="form-control" id="<?= $identificador ?>" name="<?= $identificador ?>">
+  <input type="text" class="form-control" id="<?= $identificador ?>" name="<?= $identificador ?>" value="<?=$value?>">
 </div>
