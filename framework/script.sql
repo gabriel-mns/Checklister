ea853a8136d15bf8c5933d6c7e1b335e41328ece
guilherme martins <guimsl2004@gmail.com>
2023-10-31 22:21:43 -0300

    inserts

--- framework/script.sql
+++ framework/script.sql
@@ -26,6 +26,7 @@ CREATE TABLE AVALIACAO(
 
 );
 
+
 CREATE TABLE CHECKLIST_ITEM (
 
     id_checklist_item int unsigned PRIMARY KEY AUTO_INCREMENT,
@@ -37,6 +38,8 @@ CREATE TABLE CHECKLIST_ITEM (
 
     FOREIGN KEY(id_checklist) REFERENCES CHECKLIST(id_checklist)
 );
+
+
 
 CREATE TABLE AVALIACAO_CHECKLIST_ITEM(
 
@@ -50,6 +53,8 @@ CREATE TABLE AVALIACAO_CHECKLIST_ITEM(
     FOREIGN KEY(id_checklist_item) REFERENCES CHECKLIST_ITEM(id_checklist_item)    
 
 );
+
+
 INSERT INTO checklist (titulo, data_hora_criacao, autor_versao, versao_checklist) 
             VALUES ("Requisitos", "2023-10-17 17:00:33", "Carlos", 1);
 INSERT INTO checklist (titulo, data_hora_criacao, autor_versao, versao_checklist) 
@@ -93,67 +98,40 @@ VALUES (2, "Caso haja, Para todos os fluxos alternativos estão identificados os
 INSERT INTO avaliacao (id_checklist, data_hora_avaliacao, versao_artefato, nome_avaliador) 
 		VALUES (1, "2023-11-05 08:07:10", "1", "Gabriel");
 INSERT INTO avaliacao (id_checklist, data_hora_avaliacao, versao_artefato, nome_avaliador) 
-		VALUES (1, "2023-11-05 08:30:10", "1", "Gabriel");
-INSERT INTO avaliacao (id_checklist, data_hora_avaliacao, versao_artefato, nome_avaliador) 
-		VALUES (1, "2023-11-05 09:13:50", "1", "Gabriel");
-INSERT INTO avaliacao (id_checklist, data_hora_avaliacao, versao_artefato, nome_avaliador) 
-		VALUES (1, "2023-11-05 10:01:00", "1", "Gabriel");
-INSERT INTO avaliacao (id_checklist, data_hora_avaliacao, versao_artefato, nome_avaliador) 
-		VALUES (1, "2023-11-05 10:59:30", "1", "Gabriel");
-INSERT INTO avaliacao (id_checklist, data_hora_avaliacao, versao_artefato, nome_avaliador) 
-		VALUES (2, "2023-11-07 11:36:25", "1", "Guilherme");
-INSERT INTO avaliacao (id_checklist, data_hora_avaliacao, versao_artefato, nome_avaliador) 
-		VALUES (1, "2023-11-15 09:30:00", "1", "Gabriel");
-INSERT INTO avaliacao (id_checklist, data_hora_avaliacao, versao_artefato, nome_avaliador) 
-		VALUES (2, "2023-11-15 10:15:20", "1", "Guilherme");
-INSERT INTO avaliacao (id_checklist, data_hora_avaliacao, versao_artefato, nome_avaliador) 
-		VALUES (1, "2023-11-15 11:20:45", "1", "Gabriel");
-INSERT INTO avaliacao (id_checklist, data_hora_avaliacao, versao_artefato, nome_avaliador) 
-		VALUES (2, "2023-11-15 12:40:30", "1", "Guilherme");
-INSERT INTO avaliacao (id_checklist, data_hora_avaliacao, versao_artefato, nome_avaliador) 
-		VALUES (1, "2023-11-15 13:55:10", "1", "Gabriel");
-INSERT INTO avaliacao (id_checklist, data_hora_avaliacao, versao_artefato, nome_avaliador) 
-		VALUES (2, "2023-11-15 14:30:55", "1", "Guilherme");
-INSERT INTO avaliacao (id_checklist, data_hora_avaliacao, versao_artefato, nome_avaliador) 
-		VALUES (1, "2023-11-16 14:31:55", "1", "Gabriel");
-INSERT INTO avaliacao (id_checklist, data_hora_avaliacao, versao_artefato, nome_avaliador) 
-		VALUES (1, "2023-11-16 15:31:55", "1", "Gabriel");
-INSERT INTO avaliacao (id_checklist, data_hora_avaliacao, versao_artefato, nome_avaliador) 
-		VALUES (1, "2023-11-16 16:31:55", "1", "Gabriel");
-INSERT INTO avaliacao (id_checklist, data_hora_avaliacao, versao_artefato, nome_avaliador) 
-		VALUES (1, "2023-11-16 16:39:14", "1", "Gabriel");
+		VALUES (2, "2023-11-05 08:30:10", "1", "Guilherme");
+
+INSERT INTO avaliacao_checklist_item (id_avaliacao, id_checklist_item, isConforme, observacao) 
+		VALUES (1, 1, false, "Requisito não está completo");
+INSERT INTO avaliacao_checklist_item (id_avaliacao, id_checklist_item, isConforme, observacao) 
+		VALUES (1, 2, true, "");
+INSERT INTO avaliacao_checklist_item (id_avaliacao, id_checklist_item, isConforme, observacao) 
+		VALUES (1, 3, false, "Requisito requer mais características únicas, vários se repetem" );
+INSERT INTO avaliacao_checklist_item (id_avaliacao, id_checklist_item, isConforme, observacao) 
+		VALUES (1, 4, true, "");
+INSERT INTO avaliacao_checklist_item (id_avaliacao, id_checklist_item, isConforme, observacao) 
+		VALUES (1, 5, false, "Muitos requisitos não deveriam existir");
+INSERT INTO avaliacao_checklist_item (id_avaliacao, id_checklist_item, isConforme, observacao) 
+		VALUES (1, 6, true, "");
+INSERT INTO avaliacao_checklist_item (id_avaliacao, id_checklist_item, isConforme, observacao) 
+		VALUES (1, 7, false, "Requisitos escrito de maneira ambígua" );
+INSERT INTO avaliacao_checklist_item (id_avaliacao, id_checklist_item, isConforme, observacao) 
+		VALUES (1, 8, true, "");
+INSERT INTO avaliacao_checklist_item (id_avaliacao, id_checklist_item, isConforme, observacao) 
+		VALUES (1, 9, false, "Requisito não haver com o escopo do projeto");
+INSERT INTO avaliacao_checklist_item (id_avaliacao, id_checklist_item, isConforme, observacao) 
+		VALUES (2, 10, true, "");
+INSERT INTO avaliacao_checklist_item (id_avaliacao, id_checklist_item, isConforme, observacao) 
+		VALUES (2, 11, true,"");
+INSERT INTO avaliacao_checklist_item (id_avaliacao, id_checklist_item, isConforme, observacao) 
+		VALUES (2, 12, false, "Difícil distinção entre ator principal e secundário, muitas vezes assumindo mesmos papeis");
+INSERT INTO avaliacao_checklist_item (id_avaliacao, id_checklist_item, isConforme, observacao) 
+		VALUES (2, 13, true, "");
+INSERT INTO avaliacao_checklist_item (id_avaliacao, id_checklist_item, isConforme, observacao) 
+		VALUES (2, 14, true, "");
+INSERT INTO avaliacao_checklist_item (id_avaliacao, id_checklist_item, isConforme, observacao) 
+		VALUES (2, 15, true, "");
+INSERT INTO avaliacao_checklist_item (id_avaliacao, id_checklist_item, isConforme, observacao) 
+		VALUES (2, 16, true, "");
 
 
 
-INSERT INTO avaliacao_checklist_item (id_avaliacao, id_checklist_item, isConforme, observacao) 
-		VALUES (1, 1, null, "Requisito não está completo");
-INSERT INTO avaliacao_checklist_item (id_avaliacao, id_checklist_item, isConforme, observacao) 
-		VALUES (2, 2, true, null);
-INSERT INTO avaliacao_checklist_item (id_avaliacao, id_checklist_item, isConforme, observacao) 
-		VALUES (3, 3, null, "Requisito requer mais características únicas, vários se repetem" );
-INSERT INTO avaliacao_checklist_item (id_avaliacao, id_checklist_item, isConforme, observacao) 
-		VALUES (4, 4, true, null);
-INSERT INTO avaliacao_checklist_item (id_avaliacao, id_checklist_item, isConforme, observacao) 
-		VALUES (5, 5, null, "Muitos requisitos não deveriam existir");
-INSERT INTO avaliacao_checklist_item (id_avaliacao, id_checklist_item, isConforme, observacao) 
-		VALUES (6, 6, true, null);
-INSERT INTO avaliacao_checklist_item (id_avaliacao, id_checklist_item, isConforme, observacao) 
-		VALUES (7, 7, null, "Requisitos escrito de maneira ambígua" );
-INSERT INTO avaliacao_checklist_item (id_avaliacao, id_checklist_item, isConforme, observacao) 
-		VALUES (8, 8, true, null);
-INSERT INTO avaliacao_checklist_item (id_avaliacao, id_checklist_item, isConforme, observacao) 
-		VALUES (9, 9, null, "Requisito não haver com o escopo do projeto");
-INSERT INTO avaliacao_checklist_item (id_avaliacao, id_checklist_item, isConforme, observacao) 
-		VALUES (10, 10, true, null);
-INSERT INTO avaliacao_checklist_item (id_avaliacao, id_checklist_item, isConforme, observacao) 
-		VALUES (11, 11, true, null);
-INSERT INTO avaliacao_checklist_item (id_avaliacao, id_checklist_item, isConforme, observacao) 
-		VALUES (12, 12, null, "Difícil distinção entre ator principal e secundário, muitas vezes assumindo mesmos papeis");
-INSERT INTO avaliacao_checklist_item (id_avaliacao, id_checklist_item, isConforme, observacao) 
-		VALUES (13, 13, true, null);
-INSERT INTO avaliacao_checklist_item (id_avaliacao, id_checklist_item, isConforme, observacao) 
-		VALUES (14, 14, true, null);
-INSERT INTO avaliacao_checklist_item (id_avaliacao, id_checklist_item, isConforme, observacao) 
-		VALUES (15, 15, true, null);
-INSERT INTO avaliacao_checklist_item (id_avaliacao, id_checklist_item, isConforme, observacao) 
-		VALUES (16, 16, true, null);
